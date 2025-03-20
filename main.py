a91cea63c6e4437bfaeb589215e80beb543efdbd
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2025-03-20 13:43:43 +0700

    feat: Bluetooth

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+from kivy.clock import Clock
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from plyer import bluetooth
 
-class SampleApp(MDApp):
-    
+class BluetoothApp(MDApp):
+
     def build(self):
-        self.appKv='''
+        # Desain UI dengan KivyMD
+        self.app_kv = '''
 MDScreen:
-    MDLabel:
-        text:'Hello,World.'
-        multiline:True
-        color:"blue"
-        halign:'center'         
-'''
-        AppScreen=Builder.load_string(self.appKv)
-        return AppScreen
+    MDBoxLayout:
+        orientation: "vertical"
+        MDLabel:
+            id: bluetooth_status
+            text: "Memeriksa status Bluetooth..."
+            halign: "center"
+            theme_text_color: "Primary"
+    '''
+        self.screen = Builder.load_string(self.app_kv)
+        # Jadwalkan pembaruan status Bluetooth secara berkala
+        Clock.schedule_interval(self.check_bluetooth_status, 1)  # Memeriksa setiap 1 detik
+        return self.screen
 
-SampleApp().run()
-    
+    def check_bluetooth_status(self, dt):
+        # Periksa status Bluetooth
+        if bluetooth.is_enabled():
+            self.screen.ids.bluetooth_status.text = "Bluetooth ON"
+        else:
+            self.screen.ids.bluetooth_status.text = "Bluetooth OFF"
+
+BluetoothApp().run()
